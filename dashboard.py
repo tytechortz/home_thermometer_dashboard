bd721485219461e33c766c5242587f2e4b0d9b93
James <tytechortz@gmail.com>
2019-01-19 14:03:42 -0700

    pandas crash course

--- dashboard.py
+++ dashboard.py
@@ -1,31 +1,7 @@
-import dash
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
-import plotly.graph_objs as go
-import pandas as pd
-import dash_table
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
 
-app = dash.Dash()
-
-df = pd.read_csv('./data/tempoct18.txt')
-
-app.layout = html.Div([
-    dcc.Graph(
-        id='tempoct18',
-        figure={
-            'data': [
-                go.Scatter(
-                    x = df[df.columns[0]],
-                    y = df[df.columns[1]],
-                    mode = 'lines'
-                    )
-                ]
-            }
-    )
-])
-
-
-if __name__ == '__main__':
-    app.run_server()
+df = pd.DataFrame(np.random.randn(100,4),columns='A B C D'.split())
+df.plot()
+plt.show()
