bc1f2de9ca22767463743e745a0e75ad0819cb4a
James <tytechortz@gmail.com>
2019-01-19 09:24:42 -0700

    automates db upload

--- upload.py
+++ upload.py
@@ -1,9 +1,11 @@
+#!/usr/bin/python3
+
 import csv, sqlite3
 import pandas as pd 
 
 df = pd.read_csv('//Users/jamesswank/tempjan19.txt')
 
-con = sqlite3.connect("temperatures.db")
+con = sqlite3.connect("/Users/jamesswank/Python_projects/home_thermometer_dashboard/temperatures.db")
 
 df.to_sql("Temps", con, if_exists='append', index=False)
 
