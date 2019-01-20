3acefae2b7fe6262ae4fb770a4d372a1e17b405a
James <tytechortz@gmail.com>
2019-01-19 18:32:05 -0700

    styles dash

--- dashboard.py
+++ dashboard.py
@@ -4,18 +4,27 @@ import dash_html_components as html
 
 app = dash.Dash()
 
+colors = {'background':'#777777', 'text':'#2d8fff' }
+
+
 app.layout = html.Div(children=[
-    html.H1('Hello Dash'),
-    html.Div('Dash: Web Dashboards with Python'),
+    html.H1('Hello Dash',style={'textAlign':'center',
+                                'color':colors['text']}),
+
     dcc.Graph(id='example',
             figure={'data':[
             {'x':[1,2,3], 'y':[4,1,2], 'type': 'bar', 'name': 'SF'},
             {'x':[1,2,3], 'y':[2,4,5], 'type': 'bar', 'name': 'NYC'},
             ],
                     'layout':{
+                    'plot_bgcolor':colors['background'],
+                    'paper_bgcolor':colors['background'],
+                    'font':{'color':colors['text']},
                     'title':'BAR PLOTS' 
                     }})
-])
+], style={'backgroundColor':colors['background']}
+
+)
 
 
 
