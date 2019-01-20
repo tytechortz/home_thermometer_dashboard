0db065fd79d421f4a77bdc63994e9bfc4b209bd6
James <tytechortz@gmail.com>
2019-01-19 17:03:19 -0700

    adds data and line2 file

--- myexample.py
+++ myexample.py
@@ -1,7 +1,7 @@
 import numpy as np 
 import pandas as pd 
 
-mat = np.arange(0,50).reshape(5,10)
+mat = np.arange(0,10).reshape(5,2)
 
-df = pd.DataFrame(data=mat)
+df = pd.DataFrame(data=mat,columns=['A','B'])
 print(df)
