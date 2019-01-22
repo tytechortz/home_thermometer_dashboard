93d0524aa7c87dd34b773e2834e78476ac54fd8a
James <tytechortz@gmail.com>
2019-01-21 21:29:53 -0700

    adds layout, starts callback

--- live_thermometer.py
+++ live_thermometer.py
@@ -6,3 +6,16 @@ import plotly.graph_objs as go
 import requests
 
 app = dash.Dash()
+
+app.layout = html.Div([
+    html.Div([
+    dcc.Graph(id='live-update-graph',style={'width':1200}),
+    dcc.Interval(
+        id='interval-component',
+        interval=60000,
+        n_intervals=0
+    )])
+])
+
+@app.callback(Output('live-update-graph', 'figure'),
+            [Input('interval-component', 'n_intervals')])
