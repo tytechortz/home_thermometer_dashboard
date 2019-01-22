5f0e0f61a5657d2c00298c7b5e6c20dc96ada78b
James <tytechortz@gmail.com>
2019-01-22 15:48:09 -0700

    adds max and min temps

--- live_thermometer.py
+++ live_thermometer.py
@@ -12,17 +12,23 @@ app = dash.Dash()
 
 app.layout = html.Div([
     html.Div([
-        id='temp_text',
-        children='Max Temp:'
-    ]),
-    html.Div([
     dcc.Graph(id='live-update-graph',style={'width':1200}),
     dcc.Interval(
         id='interval-component',
         interval=900000,
         n_intervals=0
-    )])
+    ),
+    html.Pre(
+        id='max_temp_text',
+        children='Max Temp:'
+    ),
+    html.Pre(
+        id='min_temp_text',
+        children='Min Temp:'
+    )]),
 ])
+
+max_temp = []
 
 @app.callback(Output('live-update-graph', 'figure'),
             [Input('interval-component', 'n_intervals')])
@@ -35,6 +41,20 @@ def update_graph(n):
             mode = 'markers+lines'
         )])
     return fig 
+
+@app.callback(Output('max_temp_text', 'children'),
+            [Input('interval-component', 'n_intervals')])
+def update_layout(n):
+    df = pd.read_csv('../../temptest.txt')
+    max_temp = df.loc[df['Y'].idxmax()]
+    return 'Max Temp:  {}'.format(max_temp)
+
+@app.callback(Output('min_temp_text', 'children'),
+            [Input('interval-component', 'n_intervals')])
+def update_layout(n):
+    df = pd.read_csv('../../temptest.txt')
+    min_temp = df.loc[df['Y'].idxmin()]
+    return 'Min Temp:  {}'.format(min_temp)
 
 if __name__ == '__main__':
     app.run_server()
