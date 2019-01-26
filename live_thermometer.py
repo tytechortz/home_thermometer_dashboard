1dc7b62d9fd2ff7f707d0ed2e5d173d7b0ec747b
James <tytechortz@gmail.com>
2019-01-26 12:45:31 -0700

    refactor

--- live_thermometer.py
+++ live_thermometer.py
@@ -8,6 +8,8 @@ import pandas as pd
 
 app = dash.Dash()
 
+df = pd.read_csv('../../temptest.txt')
+
 
 
 app.layout = html.Div([
@@ -15,20 +17,13 @@ app.layout = html.Div([
     dcc.Graph(id='live-update-graph',style={'width':1200}),
     dcc.Interval(
         id='interval-component',
-        interval=900000,
+        interval=60000,
         n_intervals=0
-    ),
-    html.Pre(
-        id='max_temp_text',
-        children='Max Temp:'
-    ),
-    html.Pre(
-        id='min_temp_text',
-        children='Min Temp:'
-    )]),
+    )])
 ])
 
-max_temp = []
+
+
 
 @app.callback(Output('live-update-graph', 'figure'),
             [Input('interval-component', 'n_intervals')])
@@ -42,19 +37,9 @@ def update_graph(n):
         )])
     return fig 
 
-@app.callback(Output('max_temp_text', 'children'),
-            [Input('interval-component', 'n_intervals')])
-def update_layout(n):
-    df = pd.read_csv('../../temptest.txt')
-    max_temp = df.loc[df['Y'].idxmax()]
-    return 'Max Temp:  {}'.format(max_temp)
 
-@app.callback(Output('min_temp_text', 'children'),
-            [Input('interval-component', 'n_intervals')])
-def update_layout(n):
-    df = pd.read_csv('../../temptest.txt')
-    min_temp = df.loc[df['Y'].idxmin()]
-    return 'Min Temp:  {}'.format(min_temp)
+
+
 
 if __name__ == '__main__':
     app.run_server()
